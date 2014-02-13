24c709297b59b1d045c192e08914dcc0a6034642
michaelfisher <fisherm@ross.gateway.2wire.net>
2014-02-13 08:20:52 -0600

    Added smooth-scroll to 'Back To Top' link.

--- content/common/footer.php
+++ content/common/footer.php
@@ -1,6 +1,6 @@
 <!--Back To Top-->
 <div id="back-to-top" class="container">
-	<a href="#home">Back To Top</a>
+	<a href="#home" class="smooth-scroll">Back To Top</a>
 </div>
 <!--End Back To Top-->
 
