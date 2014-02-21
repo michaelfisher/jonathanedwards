614c58deb8a299e4f045b7f8a20d338932a31e0e
Michael Fisher <michael@thefisherhome.net>
2014-02-21 01:31:57 -0600

    Added, but hid, site credit in footer.

--- content/common/footer.php
+++ content/common/footer.php
@@ -1,6 +1,16 @@
 <!--Copyright-->
 <div id="copyright" class="container">
-	<img src="img/logos/brand.png">
+	<div id="footer-logo" class="col-xs-6">
+		<img src="img/logos/brand.png">
+	</div>
+	<div id="site-credit" class="col-xs-6">
+		<script>
+			var user =	"michael";
+			var host =	"mfisherconcepts.com";
+			var linktext = "mfisherconcepts";
+			document.write("<a class='pull-right' href='" + "mail" + "to:" + user + "@" + host + "'>" + linktext + "</a>");
+		</script>
+	</div>
 </div>
 <!--End Back To Top-->
 
