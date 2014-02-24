e06f291d5f990ccf2f4df87262e8818e6337246d
Michael Fisher <michael@thefisherhome.net>
2014-02-24 09:31:13 -0600

    Added fade-in feature to Endorsements section.

--- js/read-more.js
+++ js/read-more.js
@@ -1,7 +1,7 @@
 $('#read-more-content').addClass('hide');
 
 $('button#read-more-toggle').on('click', function() {
-    $('#read-more-content').toggleClass('hide');
+    $('#read-more-content').fadeToggle().toggleClass('hide');
     $(this).html($(this).html() == "Less" ? "More" : "Less");
     $('[data-spy="scroll"]').each(function () {
          var $spy = $(this).scrollspy('refresh')
