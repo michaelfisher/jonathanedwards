0f40eb38036d29636b1fbbaba8356538b29eed04
Michael Fisher <michael@thefisherhome.net>
2014-05-03 07:19:27 -0500

    Finished this round of aesthetic revisions.

--- content/common/footer.php
+++ content/common/footer.php
@@ -1,7 +1,14 @@
-<!--Copyright-->
-<div id="copyright" class="container">
-	<div id="footer-logo" class="col-xs-6">
+<!--Footer-->
+<div id="footer" class="container">
+	<div id="footer-left" class="pull-left">
 		<img src="img/logos/brand.png">
+		<p id="copyright-text">
+			Copyright &copy; 2014 by Jonathan Edwards. All rights reserved.<br>
+			Find out more about City Church: <a href="http://borocitychurch.com">www.BoroCityChurch.com</a>
+		</p>
+	</div>
+	<div id="footer-right" class="pull-right">
+		<a href="http://borocitychurch.com"><img id="city-church-logo" src="img/logos/city-church-logo.png"></a>
 	</div>
 	<div id="site-credit" class="col-xs-6">
 		<script>
