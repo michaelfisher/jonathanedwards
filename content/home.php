070b74554a51e5f5f0c657340b84d4abbbd224be
Michael Fisher <michael@thefisherhome.net>
2014-05-04 07:29:27 -0500

    Updated to Jonathan's PayPal button.

--- content/home.php
+++ content/home.php
@@ -7,7 +7,7 @@
 
 		<div id="home-buttons" class="container">
 			<ul>
-				<li class="col-sm-6 col-xs-12"><a id="order-now-button" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LGGQYR9UAFTYQ" class="btn btn-block btn-lg btn-default"><span class="glyphicon glyphicon-shopping-cart"></span> Pre-Order</a></li>
+				<li class="col-sm-6 col-xs-12"><a id="order-now-button" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KAF3ZMR63QK56" class="btn btn-block btn-lg btn-default"><span class="glyphicon glyphicon-shopping-cart"></span> Pre-Order</a></li>
 				<li class="col-sm-6 col-xs-12"><a target="_blank" href="dl/left-free-preview.pdf" id="download-preview-button" class="btn btn-block btn-lg btn-default"><span class="fa fa-book"></span> Free Chapter</a></li>
 			</ul>
 		</div>
--- content/purchase.php
+++ content/purchase.php
@@ -7,7 +7,7 @@
 
 	<div id="purchase-buttons" class="container">
 		<ul>
-			<li class="col-sm-push-3 col-sm-6 col-xs-12"><a class="btn btn-block btn-lg btn-default" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=G5EWSWVLKYMPC"><span class="glyphicon glyphicon-shopping-cart"></span> Pre-Order</a></li>
+			<li class="col-sm-push-3 col-sm-6 col-xs-12"><a class="btn btn-block btn-lg btn-default" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KAF3ZMR63QK56"><span class="glyphicon glyphicon-shopping-cart"></span> Pre-Order</a></li>
 			<!--<li class="col-sm-6 col-xs-12"><a class="btn btn-block btn-lg btn-default">Ebook</a></li>-->
 		</ul>
 	</div>
