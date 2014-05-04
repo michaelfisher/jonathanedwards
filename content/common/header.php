b75e50f26656a4463d0aaba64d79bb5a9912e2d2
Michael Fisher <michael@thefisherhome.net>
2014-05-04 08:40:38 -0500

    Updated OG info.

--- content/common/header.php
+++ content/common/header.php
@@ -10,7 +10,7 @@
 		<link rel="shortcut icon" href="favicon.png">
 
 		<meta property="og:image" content="http://mfisherconcepts.com/sandbox/jonathanedwards/img/content/book-share-image.png" />
-		<meta property="og:url" content="http://mfisherconcepts.com/sandbox/jonathanedwards" />
+		<meta property="og:url" content="http://mfisherconcepts.com/sandbox/jonathanedwards/" />
 		<meta property="og:title" content="LEFT | Jonathan Edwards" />
 		<meta property="og:description" content="Just like that, he was gone. And just like that, there I was...left. The struggle to make sense of life when a parent leaves." />
 
