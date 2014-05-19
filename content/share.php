7d1ab5f1719d9ae293767ebf33c7b5231170481c
Michael Fisher <michael@thefisherhome.net>
2014-05-19 17:23:28 -0500

    More file cleanup.

--- content/share.php
+++ content/share.php
@@ -1,5 +1,3 @@
-<?php $tweet = urlencode('Check out LEFT by Jonathan Edwards #YepYep'); ?> 
-
 <!--Share-->
 <div id="share" class="col-xs-12">
 
@@ -10,7 +8,7 @@
 	<div id="share-buttons" class="container">
 		<ul>
 			<li class="col-sm-4 col-xs-12"><a id="facebook-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://www.facebook.com/sharer.php?u=http://left-book.com">Facebook</a></li>
-			<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=<?php echo $tweet; ?>">Twitter</a></li>
+			<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=TEST">Twitter</a></li>
 			<li class="col-sm-4 col-xs-12"><a id="pinterest-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://pinterest.com/pin/create/button/?url=http://left-book.com&media=http://left-book.com/img/content/book.png">Pinterest</a></li>
 		</ul>
 	</div>
--- content/thanks.php
+++ content/thanks.php
@@ -1,11 +1,10 @@
-<?php $tweet = urlencode('Check out LEFT by Jonathan Edwards #YepYep'); ?>
-
-<!--Why-->
+<!--Thanks-->
 <div id="thanks" class="col-xs-12">
 
 	<div id="thanks-header" class="col-xs-12">
 		<h1>Thank You!</h1>
 		<h3>Your order has been placed, and a receipt will be emailed to you.</h3>
+		<p class="help-block">Pre-orders will begin shipping on XX/XX.</p>
 		<h3 id="social-request">Would you mind taking a moment to help spread the word about <strong><em>LEFT</em></strong> on social media?</h3>
 	</div>
 
@@ -13,7 +12,7 @@
 		<div id="thanks-buttons" class="container">
 			<ul>
 				<li class="col-sm-4 col-xs-12"><a id="facebook-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://www.facebook.com/sharer.php?u=http://left-book.com">Facebook</a></li>
-				<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=<?php echo $tweet; ?>">Twitter</a></li>
+				<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=TEST">Twitter</a></li>
 				<li class="col-sm-4 col-xs-12"><a id="pinterest-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://pinterest.com/pin/create/button/?url=http://left-book.com&media=http://left-book.com/img/content/book.png">Pinterest</a></li>
 			</ul>
 		</div>
@@ -22,4 +21,4 @@
 
 </div>
 
-<!--End Why-->
+<!--End Thanks-->
