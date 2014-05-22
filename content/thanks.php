197deb1b8a3093d0767111558daf7df698c45f8d
Michael Fisher <michael@thefisherhome.net>
2014-05-22 15:27:36 -0500

    Twitter text.

--- content/thanks.php
+++ content/thanks.php
@@ -12,7 +12,7 @@
 		<div id="thanks-buttons" class="container">
 			<ul>
 				<li class="col-sm-4 col-xs-12"><a id="facebook-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://www.facebook.com/sharer.php?u=http://left-book.com">Facebook</a></li>
-				<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=Just+purchased+LEFT%3A+The+Struggle+to+Make+Sense+of+Life+When+a+Parent+Leaves+by+%40notthepuritan%21+Excited+to+read%21">Twitter</a></li>
+				<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=Just+purchased+LEFT%3A+The+Struggle+to+Make+Sense+of+Life+When+a+Parent+Leaves+by+%40notthepuritan%21+Excited+to+read%21&amp;source=webclient">Twitter</a></li>
 				<li class="col-sm-4 col-xs-12"><a id="pinterest-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://pinterest.com/pin/create/button/?url=http://left-book.com&media=http://left-book.com/img/content/book.png">Pinterest</a></li>
 			</ul>
 		</div>
