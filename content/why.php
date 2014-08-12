2b1447fc9a0f96a79918debfcbce15aa6e9ade9e
Michael Fisher <michael@thefisherhome.net>
2014-08-11 22:05:19 -0500

    Icon change and wording of buttons.

--- content/why.php
+++ content/why.php
@@ -28,8 +28,8 @@
 	<div class="col-xs-12">
 		<div id="why-buttons" class="container">
 			<ul>
-				<li class="col-sm-4 col-xs-12"><a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KAF3ZMR63QK56" id="order-now-button" class="btn btn-block btn-lg btn-default"><span class="glyphicon glyphicon-shopping-cart"></span> Order Now</a></li>
-				<li class="col-sm-4 col-xs-12"><a target="_blank" href="http://www.amazon.com/LEFT-Struggle-Sense-Parent-Leaves-ebook/dp/B00MMMF9LY/" id="order-ebook-button" class="btn btn-block btn-lg btn-default"><span class="fa fa-book"></span> Buy Ebook</a></li>
+				<li class="col-sm-4 col-xs-12"><a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KAF3ZMR63QK56" id="order-now-button" class="btn btn-block btn-lg btn-default"><span class="fa fa-book"></span> Hard Copy</a></li>
+				<li class="col-sm-4 col-xs-12"><a target="_blank" href="http://www.amazon.com/LEFT-Struggle-Sense-Parent-Leaves-ebook/dp/B00MMMF9LY/" id="order-ebook-button" class="btn btn-block btn-lg btn-default"><span class="fa fa-tablet"></span> Ebook</a></li>
 				<li class="col-sm-4 col-xs-12"><a target="_blank" href="dl/left-free-preview.pdf" id="download-preview-button" class="btn btn-block btn-lg btn-default"><span class="fa fa-cloud-download"></span> Free Chapter</a></li>
 			</ul>
 		</div>
