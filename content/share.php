712f5cf3714d0192bbb3f4e03d04f22e6de707a8
Michael Fisher <michael@thefisherhome.net>
2014-09-01 15:46:42 -0500

    Removed shipping date from Twitter share button.

--- content/share.php
+++ content/share.php
@@ -8,7 +8,7 @@
 	<div id="share-buttons" class="container">
 		<ul>
 			<li class="col-sm-4 col-xs-12"><a id="facebook-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://www.facebook.com/sharer.php?u=http://left-book.com">Facebook</a></li>
-			<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=Looking+forward+to+reading+LEFT%3A+The+Struggle+to+Make+Sense+of+Life+When+a+Parent+Leaves+by+%40notthepuritan%21+9.16.14&amp;source=webclient">Twitter</a></li>
+			<li class="col-sm-4 col-xs-12"><a id="twitter-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://twitter.com/intent/tweet?url=http://left-book.com&text=Looking+forward+to+reading+LEFT%3A+The+Struggle+to+Make+Sense+of+Life+When+a+Parent+Leaves+by+%40notthepuritan%21&amp;source=webclient">Twitter</a></li>
 			<li class="col-sm-4 col-xs-12"><a id="pinterest-share-button" class="btn btn-block btn-lg btn-default" target="_blank" href="http://pinterest.com/pin/create/button/?url=http://left-book.com&media=http://left-book.com/img/content/book.png">Pinterest</a></li>
 		</ul>
 	</div>
